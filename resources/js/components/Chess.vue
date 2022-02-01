96808a8bb731d1508bcdd5ac1d0527227b942738
Dante2001 <andrianodia@hotmail.com>
2022-02-01 17:18:07 -0300

    parseo de juegos

--- resources/js/components/Chess.vue
+++ resources/js/components/Chess.vue
@@ -8,15 +8,15 @@
 import {chessboard} from 'vue-chessboard'
 import 'vue-chessboard/dist/vue-chessboard.css'
 
-
 export default {
     components:{
         chessboard
     },
     mounted() {
-        var Chess = require('chess.js/chess.js');
-        var chessGame = new Chess();
-        console.log(chessGame);
+
+
+
+
     },
 
 }
--- resources/js/components/SyncAccount.vue
+++ resources/js/components/SyncAccount.vue
@@ -15,20 +15,51 @@
 
 <script>
 
-export default {
-    data() {
-        return {
-            account: "",
+    import '../classes.js';
+
+
+    export default {
+        data() {
+            return {
+                account: "",
+                chessGames: [],
+                chessGamesParsed: [],
+                movementMatrix:[[]],
+            }
+        },
+        methods: {
+            getGames() {
+                axios
+                    .get('https://lichess.org/api/games/user/' + this.account + '?max=3&evals=true')
+                    .then(response => {
+                        // var Chess = require('chess.js/chess.js');
+                        // var chessGame = new Chess();
+                        // console.log(chessGame);
+
+                        console.log(response)
+                        this.chessGames = response.data;
+                        this.chessGames = this.chessGames.split("\n\n");
+                        this.chessGamesParsed = [];
+                        for(var i=1; i<this.chessGames.length; i+=2) {
+                            this.chessGames[i] = this.chessGames[i].replaceAll('{','');
+                            this.chessGames[i] = this.chessGames[i].replaceAll('}','');
+                            this.chessGamesParsed.push(this.chessGames[i]);
+                        }
+
+                        for(var i=0; i<this.chessGamesParsed.length; i++) {
+                            console.log(i);
+                            var gameMovement = [];
+
+                            for (var j=0; j<gameMovement.length; j++) {
+                                // if (this.movementMatrix)
+                            }
+                        }
+
+
+                    })
+
+            }
         }
-    },
-    methods: {
-        getGames() {
-            axios.get('https://lichess.org/api/games/user/' + this.account + '?max=3')
-                .then(response => {
-                    console.log(response)
-                })
-        }
+        ,
     }
-    ,
-}
 </script>
