8de60e6ece86c4d34b6e53522003cfb2d34a76f7
Diego Andriano <andrianodna@gmail.com>
2022-02-02 10:10:07 -0300

    sync account como componente de chess

--- resources/js/components/Chess.vue
+++ resources/js/components/Chess.vue
@@ -1,16 +1,20 @@
 <template>
     <div>
+        <sync-account />
+
         <chessboard />
     </div>
 </template>
 
 <script>
+import SyncAccount from './SyncAccount'
 import {chessboard} from 'vue-chessboard'
 import 'vue-chessboard/dist/vue-chessboard.css'
 
 export default {
     components:{
-        chessboard
+        chessboard,
+        'sync-account': SyncAccount,
     },
     mounted() {
 
