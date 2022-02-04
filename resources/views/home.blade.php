2df200b7a316019a29c1f307d892d7194709ffca
Diego Andriano <andrianodna@gmail.com>
2022-02-04 14:35:11 -0300

    wip

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,27 +5,7 @@
             <div class="font-bold text-xl">
                 Chess Common Mistakes
             </div>
-
-            <li class="flex">
-                <ul class="text-lg px-10 py-4 text-green-500 font-semibold cursor-pointer">
-                    Inicio
-                </ul>
-                <ul class="text-lg px-10 py-4 text-black font-semibold cursor-pointer">
-                    Uno
-                </ul>
-                <ul class="text-lg px-10 py-4 text-black font-semibold cursor-pointer">
-                    Otro
-                </ul>
-                <ul class="text-lg px-10 py-4 text-black font-semibold cursor-pointer">
-                    Ese
-                </ul>
-                <ul class="text-lg px-10 py-4 text-black font-semibold cursor-pointer">
-                    Aquél
-                </ul>
-            </li>
-
-            <p class="text-lg px-8 py-4 text-black-500 border-2 border-green-500 font-semibold cursor-pointer">Contacto</p>
-
+            
         </div>
     </div>
 
