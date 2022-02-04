d3ded103923874a7b76616e2857c97ebf805f778
Dante2001 <andrianodia@hotmail.com>
2022-02-04 15:03:17 -0300

    creditos

--- resources/js/components/SyncAccount.vue
+++ resources/js/components/SyncAccount.vue
@@ -215,3 +215,4 @@ export default {
     ,
 }
 </script>
+
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,26 @@
 <x-app>
-    <div id="app" class="bg-white py-3">
+    <div id="app" class="bg-white py-3 mt-3">
         <div class="flex justify-around">
 
             <div class="font-bold text-xl">
                 Chess Common Mistakes
             </div>
-            
+
+            <div>
+                <a class="text-xl font-bold hover:underline" href="https://github.com/DiegoAndriano/ChessCommonMistakes/">
+                    Repository
+                </a>
+            </div>
+
         </div>
     </div>
 
+
+
     <div class="max-w-4xl mx-auto">
+        <div style="padding:10px; border-radius:10px; color:#fff; background-color:#333; text-align:center;">
+            <a href="mailto:andrianodna@gmail.com">Made by the Filocode team! | andrianodna@gmail.com</a>
+        </div>
         <h2 class="font-bold text-3xl mt-6">Select your account</h2>
         <p class="mb-4">Get your most common errors in the openings given a number of games.</p>
 
